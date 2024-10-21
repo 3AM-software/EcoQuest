55bf39b359279916af6457da562608b171eef7e8
Brayden Watt <icedragon2351@gmail.com>
2024-10-21 14:38:13 -0400

    FIrst Commit

---
+++ EcoQuest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  EcoQuest
+//
+//  Created by Brayden Watt on 10/21/24.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Hello, World!")
+            Text("This is a VStack!")
+            Image(systemName: "star")
+        }
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ EcoQuest/EcoQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoQuestApp.swift
+//  EcoQuest
+//
+//  Created by Brayden Watt on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct EcoQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
